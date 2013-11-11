217640c98a496c4af32afaa65e5ff33b7446b80c
Allon Hadaya <allon@ahadaya.com>
2013-11-11 18:22:31 +0000

    Write a program that rings the bell on downbeats at a certain bpm for a certain polyrhythm.

---
+++ Bell.hs
@@ -0,0 +1,17 @@
+import Control.Concurrent
+import Data.List
+import Polyrhythm
+import System.Environment
+
+main = do
+  (bpm:rhythms) <- getArgs
+  let delay = quot (1000000 * 60) (read bpm)
+  let down_beats = downs $ map read rhythms
+  mapM_ output $ zip (map signal down_beats) (repeat delay)
+
+output (message, delay) = do
+  putStrLn message
+  threadDelay delay
+
+signal True = "\a!"
+signal False = "."
