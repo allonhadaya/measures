3c9e09a7e85c0d3ab8418c2b8a5964fe426c4caf
Allon Hadaya <allon@ahadaya.com>
2013-11-11 18:21:09 +0000

    Add type declarations to exported functions.

--- Polyrhythm.hs
+++ Polyrhythm.hs
@@ -3,6 +3,7 @@ module Polyrhythm (gaps, ups, downs) where
 import Data.List
 
 -- counts up beat gaps for a polyrhythm xs
+gaps :: [Int] -> [Int]
 gaps xs = (tail . count_gap) $ downs xs
 
 -- counts up beats between down beats
@@ -10,9 +11,11 @@ count_gap (True:xs) = 0 : count_gap xs
 count_gap (False:xs) = let (u:us) = count_gap xs in (1 + u):us
 
 -- marks the up beats in a polyrhythm
+ups :: [Int] -> [Bool]
 ups xs = map not $ downs xs
 
 -- marks the down beats in a polyrhythm
+downs :: [Int] -> [Bool]
 downs xs = map (any (\x -> x == 1)) $ count xs
 
 -- builds up parallel counts
